88dcfbe7762a7b1fd357fb8466ca8746ce985798
briendeau <yamori425@gmail.com>
2022-12-31 03:01:08 -0500

    adding db

--- server.js
+++ server.js
@@ -12,11 +12,9 @@ const image = require('./controllers/image');
 const db = knex({
   // connect to your own database here:
   client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
   }
 });
 
