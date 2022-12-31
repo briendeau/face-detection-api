588be35da4af629c8fd7ded901aba6d69857b5f9
briendeau <yamori425@gmail.com>
2022-12-31 03:23:26 -0500

    adding db

--- server.js
+++ server.js
@@ -9,11 +9,12 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 const db = knex({
   // connect to your own database here:
   client: 'pg',
   connection: {
-    connectionString : process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true,
   }
 });
