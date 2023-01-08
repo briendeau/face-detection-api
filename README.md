f86faf625b449aaeda844fae0b6715e66df60610
briendeau <yamori425@gmail.com>
2023-01-08 18:13:50 -0500

    deploying backend api as a copy

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-1. Clone this repo
+Created by Brian Riendeau
+This is the backend api for the face-detection app using artificial intelligence to detect faces in pictures.
+
+1. First clone this repo
 2. Run `npm install`
 3. Run `npm start`
+4. Use your own API key at Clarifai
--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const db = knex({
 const app = express();
 
 app.use(cors())
-app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
+app.use(express.json()); // latest version of expressJS now comes with Body-Parser
 console.log("it is working.");
 
 app.get('/', (req, res)=> { res.send(db.users) })
